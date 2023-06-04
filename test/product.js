69d176d4555a82ceabb1017913ee95be0073ff1e
cristian1534 <cmachuca32@gmail.com>
2023-06-04 05:38:09 +0000

    Updating dependencies CI.

--- test/product.js
+++ test/product.js
@@ -1,6 +1,6 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const server = require("../../server");
+const server = require("../src/server");
 
 chai.use(chaiHttp);
 const should = chai.should();
